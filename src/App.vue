09e43040755d4925ae9251613b58274af89b11c7
Aldrian <himalosc@gmail.com>
2022-04-27 23:44:14 +0800

    init push for this vue project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <header-page/>
+  <main>
+    <home-page />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import HeaderPage from './components/HeaderPage.vue';
+import HomePage from './components/HomePage.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    HeaderPage,
+    HomePage,
   },
 };
 </script>
 
 <style>
+*, ::before, ::after {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  background-color: rgba( 44, 44, 44, 1 );
+}
+
+.fd {
+  display: flex;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+header, main {
+  margin: 0 auto;
+  width: 95%;
+  max-width: 1024px;
+}
+
+main {
+  background-color: rgba( 234, 234, 234, 1 );
+  padding: 20px;
 }
 </style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,31 @@
+<template>
+    <header>
+        <nav>
+            <ul class="fd">
+                <li class="nav-item">
+                    first nav item
+                </li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<script>
+export default {
+  name: 'HeaderPage',
+};
+</script>
+
+<style scoped>
+header {
+    background-color: rgba( 214, 234, 214, 1 );
+    padding: 5px 10px;
+    margin-top: 10px;
+    margin-bottom: 20px;
+}
+
+li.nav-item {
+    background-color: red;
+    height: 40px;
+}
+</style>
